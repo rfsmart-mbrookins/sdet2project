a552e3d8f4768311630dedd0161059228cf4ed8a
Michael Brookins <michael.brookins@rfsmart.com>
2024-06-19 15:16:21 -0400

    locators and such

---
+++ tests/login_demo.spec.js
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+ test('Demo Login Test 1', async({page}) => {
+    await page.goto('https://demo.applitools.com/')
+    // await page.pause()
+    await page.locator('[placeholder="Enter your username"]').fill('Raghav')
+    await page.locator('[placeholder="Enter your password"]').fill('Password1')
+    await page.locator('text=Sign in').click()
+    await page.waitForSelector('text=Sign in', {timeout: 5000})
+    await page.locator('test=ACME').isVisible()
+
+ });
+
+ test.only('Demo Login Test 2', async({page}) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php')
+    // await page.pause()
+    await page.locator('[placeholder="Username"]').click()
+    await page.locator('[placeholder="Username"]').fill('Admin')
+    await page.locator('[placeholder="Username"]').press('Tab')
+    await page.locator('[placeholder="Password"]').fill('admin123')
+    await page.locator('button:has-text("Login")').click()
+    await page.waitForURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
+    await page.locator('p:has-text("Test 22 LastName")').click()
+    await page.locator('text=Logout').click()
+    await page.waitForURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+ });
+
+ 
--- tests/selectors.spec.js
+++ tests/selectors.spec.js
@@ -6,5 +6,14 @@ import { test, expect } from '@playwright/test';
    await page.locator('id=user-name').fill('Edison')
    await page.locator('[id="user-name"]').fill('Einstein')
    await page.pause()
+   //use css selector 
+   await page.locator('#login-button').click()
+   await page.pause()
+   //using xpath
+   await page.locator('xpath=//input[@name="login-button"])').click()
+   await page.locator('//input[@name="login-button"]').click()
+   //using text
+   await page.locator('text=LOGIN').click()
+   await page.locator(':has-text("LOGIN")').click()
    
  });
