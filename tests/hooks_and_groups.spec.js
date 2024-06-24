dd06455566ee582b4fce69b8eef78c5bfb531192
Michael Brookins <michael.brookins@rfsmart.com>
2024-06-24 07:51:43 -0400

    hooks and groups wip

---
+++ tests/hooks_and_groups.spec.js
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+
+test('login', async({page}) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-button"]').click();
+    await page.waitForURL('https://www.saucedemo.com/inventory.html');
+        await page.close();
+    
+
+});
+
+test.only('homepage', async({page}) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-button"]').click();
+    await page.waitForURL('https://www.saucedemo.com/inventory.html');
+    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+    await page.locator('[data-test="item-1-title-link"]').click();
+    await page.locator('[data-test="add-to-cart"]').click();
+        await page.close();
+});
+
+test('logout', async({page}) => {
+    
+})
